73d685b60a47779494a521b79afd18d760c05fa6
naari3 <naari.named@gmail.com>
2021-01-14 14:20:11 +0900

    Support twitter

--- src/contentScripts.ts
+++ src/contentScripts.ts
@@ -1,4 +1,8 @@
-document.body.innerHTML = document.body.innerHTML.replace(/ドラ/g, "とげ");
-document.body.innerHTML = document.body.innerHTML.replace(/どら/g, "とげ");
-document.title = document.title.replace(/ドラ/g, "とげ");
-document.title = document.title.replace(/どら/g, "とげ");
+if (!location.href.includes("twitter.com")) {
+    document.body.innerHTML = document.body.innerHTML.replace(/ドラ|どら/g, "とげ");
+    document.title = document.title.replace(/ドラ|どら/g, "とげ");
+} else {
+    setInterval(() => document.querySelectorAll("span").forEach(s => {
+        s.innerText = s.innerText.replace(/ドラ|どら/g, "とげ");
+    }), 1000);
+}
