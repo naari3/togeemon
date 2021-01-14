b0081dc1811620cefed280908b8d19c51d164c59
naari3 <naari.named@gmail.com>
2021-01-14 13:56:39 +0900

    Implement ドラ → とげ

---
+++ src/contentScripts.ts
@@ -0,0 +1,2 @@
+document.body.innerHTML = document.body.innerHTML.replace(/ドラ/g, "とげ");
+document.title = document.title.replace(/ドラ/g, "とげ");
--- webpack.common.js
+++ webpack.common.js
@@ -2,8 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    popup: path.join(__dirname, "src/popup/index.tsx"),
-    eventPage: path.join(__dirname, "src/eventPage.ts")
+    contentScripts: path.join(__dirname, "src/contentScripts.ts")
   },
   output: {
     path: path.join(__dirname, "dist/js"),
@@ -15,25 +14,10 @@ module.exports = {
         exclude: /node_modules/,
         test: /\.tsx?$/,
         use: "ts-loader"
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader" // Creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader" // Translates CSS into CommonJS
-          },
-          {
-            loader: "sass-loader" // Compiles Sass to CSS
-          }
-        ]
       }
     ]
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: [".ts", ".js"]
   }
 };
